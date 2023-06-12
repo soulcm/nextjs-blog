ff4e4c40d4a69d21d28dd7f0221a8f27437d7242
季陆 <5945lcm@gmail.com>
2023-06-12 15:25:12 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,9 @@
-
+self.addEventListener('push', async function (e) {
+  var data = await e.data.text();
+  console.log('push data :>> ', data);
+  if (data) {
+    self.registration.showNotification(data);
+  } else {
+    console.log('没有 push 任何数据')
+  }
+})
